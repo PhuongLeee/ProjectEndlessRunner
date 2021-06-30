d98a526d39c7fb2dacc8769f1ae769ff9fc956d1
F:\MySetup\MyApplication\GitHub\SetUp <lepg.hn@gmail.com>
2021-06-30 11:42:55 +0700

    6. save data by firebase

---
+++ Project/EndlessRunnerProject/Assets/Scripts/FirebaseData/DataController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase.Database;
+
+public class DataController : MonoBehaviour
+{
+    private DatabaseReference reference;
+    private string idUser;
+
+    private void Awake() {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start() {
+        // Get the root reference location of the database.
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+#if UNITY_IOS
+        idUser = UIDevice.identifierForVendor;
+#else
+        idUser = SystemInfo.deviceUniqueIdentifier;
+#endif
+
+    }
+
+    public void SaveData() {
+        var track = TrackManager.instance;
+        if (track == null) return;
+        User user = new User(idUser, TrackManager.instance.score);
+        string json = JsonUtility.ToJson(user);
+
+        reference.Child("users").Child(user.id).SetRawJsonValueAsync(json).ContinueWith(task=> {
+            if(task.IsCompleted) {
+                Debug.Log("save data complete");
+            }else {
+                Debug.Log("save data not complete");
+            }
+        });
+    }
+
+    private void OnApplicationQuit() {
+        SaveData();
+    }
+
+}
---
+++ Project/EndlessRunnerProject/Assets/Scripts/FirebaseData/User.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class User
+{
+    public string id;
+    public int score;
+
+    public User(string id, int score) {
+        this.id = id;
+        this.score = score;
+    }
+}
